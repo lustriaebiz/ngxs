5521d1536799a363ee4a34500793740aae79d3b1
Ebiz <lustriaebiz.cakap@gmail.com>
2019-11-08 14:56:39 +0700

    state

---
+++ src/app/state/animal.state.ts
@@ -0,0 +1,40 @@
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+import { Animal } from "../interfaces/animal";
+import { AddAnimal, RemoveAnimal } from '../actions/animal.action'
+
+export class AnimalStateModel {
+    animals: Animal[];
+}
+
+@State<AnimalStateModel>({
+    name: 'animals',
+    defaults: {
+        animals: []
+    }
+})
+
+export class AnimalState {
+
+    // Section 4
+    @Selector()
+    static getTutorials(state: AnimalStateModel) {
+        return state.animals
+    }
+
+    // Section 5
+    @Action(AddAnimal)
+    add({getState, patchState }: StateContext<AnimalStateModel>, { payload }:AddAnimal) {
+        const state = getState();
+        patchState({
+            animals: [...state.animals, payload]
+        })
+    }
+
+    @Action(RemoveAnimal)
+    remove({getState, patchState }: StateContext<AnimalStateModel>, { payload }:RemoveAnimal) {
+        patchState({
+            animals: getState().animals.filter(a => a.name != payload)
+        })
+    }
+
+}
